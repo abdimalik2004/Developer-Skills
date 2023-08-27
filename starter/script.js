b2d0e85601516a6c0f99418379392a1930c8224e
abdimalik abdirahman <mahadalleabdimalik@gmail.com>
2023-08-27 17:20:09 +0300

    Added

---
+++ starter/script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+const x = '23';
+if (x === 23) console.log(23);
+
+const calcAge = birthYear => 2023 - birthYear;
+console.log(calcAge(1991));
